4f8ca83b87f7a1ba3d10413cc911a58b1aeb3be4
ryomaruN <rnakamaru140@gmail.com>
2022-04-24 00:12:55 +0900

    use GCP

--- src/main.go
+++ src/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	texttospeech "cloud.google.com/go/texttospeech/apiv1"
+	texttospeechpb "google.golang.org/genproto/googleapis/cloud/texttospeech/v1"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	ctx := context.Background()
+
+	client, err := texttospeech.NewClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	req := texttospeechpb.SynthesizeSpeechRequest{
+		Input: &texttospeechpb.SynthesisInput{
+			InputSource: &texttospeechpb.SynthesisInput_Text{Text: "こんにちは"},
+		},
+		Voice: &texttospeechpb.VoiceSelectionParams{
+			LanguageCode: "ja-JP",
+			SsmlGender:   texttospeechpb.SsmlVoiceGender_FEMALE,
+		},
+		AudioConfig: &texttospeechpb.AudioConfig{
+			AudioEncoding: texttospeechpb.AudioEncoding_MP3,
+		},
+	}
+
+	resp, err := client.SynthesizeSpeech(ctx, &req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filename := "output.mp3"
+	err = ioutil.WriteFile(filename, resp.AudioContent, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Audio content written to file: %v\n", filename)
 }
