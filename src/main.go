b7cf189e8c62427b33b138c1ba2a5502816dcb1a
ryomaruN <rnakamaru140@gmail.com>
2022-04-26 01:26:34 +0900

    slight change

--- src/main.go
+++ src/main.go
@@ -6,11 +6,26 @@ import (
 	"io/ioutil"
 	"log"
 
+	"github.com/joho/godotenv"
+
 	texttospeech "cloud.google.com/go/texttospeech/apiv1"
 	texttospeechpb "google.golang.org/genproto/googleapis/cloud/texttospeech/v1"
 )
 
 func main() {
+
+}
+
+func loadEnv() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
+
+func fetchTextToSpeech(text string) {
 	ctx := context.Background()
 
 	client, err := texttospeech.NewClient(ctx)
@@ -21,7 +36,7 @@ func main() {
 
 	req := texttospeechpb.SynthesizeSpeechRequest{
 		Input: &texttospeechpb.SynthesisInput{
-			InputSource: &texttospeechpb.SynthesisInput_Text{Text: "こんにちは"},
+			InputSource: &texttospeechpb.SynthesisInput_Text{Text: text},
 		},
 		Voice: &texttospeechpb.VoiceSelectionParams{
 			LanguageCode: "ja-JP",
