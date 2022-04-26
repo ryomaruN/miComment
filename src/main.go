8baa0fcfe522f4c3ed36aed827366ade24c5b9dd
ryomaruN <rnakamaru140@gmail.com>
2022-04-26 23:52:39 +0900

    First Echo Hello World!

--- src/main.go
+++ src/main.go
@@ -1,20 +1,62 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
+)
 
-	texttospeech "cloud.google.com/go/texttospeech/apiv1"
-	texttospeechpb "google.golang.org/genproto/googleapis/cloud/texttospeech/v1"
+var (
+	// vcsession         *discordgo.VoiceConnection
+	HelloWorld        = "helloworld"
+	ChannelVoiceJoin  = "vcjoin"
+	ChannelVoiceLeave = "vcleave"
 )
 
 func main() {
+	loadEnv()
+
+	discord, err := discordgo.New("Bot " + os.Getenv("BOT_TOKEN"))
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	discord.AddHandler(onMessageCreate)
+
+	err = discord.Open()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Listening...")
+
+	// channel作成
+	stopBot := make(chan os.Signal, 1)
+
+	// OSからのシグナルをキャッチする？
+	signal.Notify(stopBot, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+
+	// 処理を堰き止める
+	<-stopBot
+
+	fmt.Println("Interrupted.")
+
+	err = discord.Close()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Closed.")
 
 }
 
@@ -24,47 +66,65 @@ func loadEnv() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	discord, err := discordgo.New("Bot " + os.Getenv("BOT_TOKEN"))
-
-	if err != nil {
-		fmt.Println(err)
-	}
-	// dummy
-	fmt.Println(discord)
-
 }
 
-func fetchTextToSpeech(text string) {
-	ctx := context.Background()
+// メッセージ受信ハンドラ
+func onMessageCreate(ses *discordgo.Session, mc *discordgo.MessageCreate) {
+	// if err != nil {
+	// 	log.Println("Error getting channel: ", err)
+	// 	return
+	// }
 
-	client, err := texttospeech.NewClient(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Close()
+	fmt.Printf("%20s %20s %20s > %s\n", mc.ChannelID, time.Now().Format(time.Stamp), mc.Author.Username, mc.Content)
 
-	req := texttospeechpb.SynthesizeSpeechRequest{
-		Input: &texttospeechpb.SynthesisInput{
-			InputSource: &texttospeechpb.SynthesisInput_Text{Text: text},
-		},
-		Voice: &texttospeechpb.VoiceSelectionParams{
-			LanguageCode: "ja-JP",
-			SsmlGender:   texttospeechpb.SsmlVoiceGender_FEMALE,
-		},
-		AudioConfig: &texttospeechpb.AudioConfig{
-			AudioEncoding: texttospeechpb.AudioEncoding_MP3,
-		},
+	switch {
+	case strings.HasPrefix(mc.Content, fmt.Sprintf("%s %s", fmt.Sprintf("<@%s>", os.Getenv("CLIENT_ID")), HelloWorld)):
+		sendMessage(ses, mc.ChannelID, "Hello World!")
 	}
-
-	resp, err := client.SynthesizeSpeech(ctx, &req)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	filename := "output.mp3"
-	err = ioutil.WriteFile(filename, resp.AudioContent, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Audio content written to file: %v\n", filename)
 }
+
+// メッセージ送信関数
+func sendMessage(s *discordgo.Session, channelID string, msg string) {
+	_, err := s.ChannelMessageSend(channelID, msg)
+
+	log.Println(">>> " + msg)
+	if err != nil {
+		log.Println("Error sending message: ", err)
+	}
+}
+
+// Cloud Text-to-Speech API呼び出し
+// func fetchTextToSpeech(text string) {
+// 	ctx := context.Background()
+
+// 	client, err := texttospeech.NewClient(ctx)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer client.Close()
+
+// 	req := texttospeechpb.SynthesizeSpeechRequest{
+// 		Input: &texttospeechpb.SynthesisInput{
+// 			InputSource: &texttospeechpb.SynthesisInput_Text{Text: text},
+// 		},
+// 		Voice: &texttospeechpb.VoiceSelectionParams{
+// 			LanguageCode: "ja-JP",
+// 			SsmlGender:   texttospeechpb.SsmlVoiceGender_FEMALE,
+// 		},
+// 		AudioConfig: &texttospeechpb.AudioConfig{
+// 			AudioEncoding: texttospeechpb.AudioEncoding_MP3,
+// 		},
+// 	}
+
+// 	resp, err := client.SynthesizeSpeech(ctx, &req)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	filename := "output.mp3"
+// 	err = ioutil.WriteFile(filename, resp.AudioContent, 0644)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	fmt.Printf("Audio content written to file: %v\n", filename)
+// }
